7d751de0675791fd9a239b35042431a7b47c5d75
Christopher Pedersen <12384786+topherPedersen@users.noreply.github.com>
2018-10-05 01:42:36 -0500

    Add files via upload

---
+++ game.py
@@ -0,0 +1,14 @@
+import coco
+# pset = coco.pset
+placeholder = coco.placeholder
+
+# pset()
+
+# Define Main Game Loop
+def mainGameLoop():
+    placeholder() # replace placeholder() function with main game loop logic
+
+# Run Main Game Loop
+run = True
+while run == True:
+	mainGameLoop()
