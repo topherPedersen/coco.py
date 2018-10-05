8477c26903ebe217d8c61d14780e3e8960d9c2a7
Christopher Pedersen <12384786+topherPedersen@users.noreply.github.com>
2018-10-05 02:49:18 -0500

    Update game.py

--- game.py
+++ game.py
@@ -7,4 +7,7 @@ set(15, 15, "white")
 # Run Main Game Loop
 run = True
 while run == True:
-	busywork() # replace busywork() with your game logic
+	# busywork() function simply adds some work for the
+	# computer to do in order to avoid the dreaded "pinwheel".
+	# When ready, replace this function with your game logic.
+	busywork() 
