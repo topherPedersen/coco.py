3a7e1cb3121267f6306411c9098165d227e13781
Christopher Pedersen <12384786+topherPedersen@users.noreply.github.com>
2018-10-04 21:26:39 -0500

    Update pset.py

--- pset.py
+++ pset.py
@@ -1,47 +1,7 @@
-'''
-# Import the Turtle 2D Graphics Module
-import turtle 
-
-# Create Game Window
-screen = turtle.Screen()
-screen.setup(600, 600)
-screen.bgcolor("green")
-
-pixel = []
-newTurtle = turtle.Turtle()
-newTurtle.hideturtle()
-newTurtle.penup()
-newTurtle.goto(-240, 240)
-newTurtle.shape("square")
-newTurtle.color("white")
-newTurtle.showturtle()
-pixel.append(newTurtle)
-
-i = 0
-while True:
-    if i % 100 == 0:
-        print("iteration count: " + str(i))
-    i = i + 1
-'''
-
-
-
 # Import the Tkinter & Turtle Graphics Modules
 import Tkinter
 import turtle
 
-'''
-# Create Game Window
-screen = turtle.Screen()
-screen.screensize(600, 600, "green")
-'''
-
-'''
-# Create Game Window
-screen = turtle.Screen()
-screen.setup(600, 600)
-screen.bgcolor("green")
-'''
 screen = turtle.Screen()
 screen.bgcolor("black")
 screen.screensize()
@@ -56,19 +16,6 @@ newTurtle.shape("square")
 newTurtle.color("white")
 newTurtle.showturtle()
 pixel.append(newTurtle)
-
-# Create Sprite
-'''
-screen.addshape("sprite.gif")
-mySprite = turtle.Turtle()
-mySprite.hideturtle() # hide turtle until the the duck image replaces the placeholder shape
-mySprite.penup() # prevent sprite from drawing on screen as it moves
-mySprite.left(90) # rotate sprite for proper placement on screen
-mySprite.shape("sprite.gif") # Set Image for Sprite
-mySprite.setposition(0, 0)
-mySprite.showturtle()
-'''
-
 
 # Main Game Loop (Keeps Game Window From Closing)
 Tkinter.mainloop()
