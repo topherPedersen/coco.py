85ea5506ee06e022fe1cfd34fdbf26d49ec9a434
Christopher Pedersen <12384786+topherPedersen@users.noreply.github.com>
2018-10-04 21:24:46 -0500

    Update pset.py

--- pset.py
+++ pset.py
@@ -1,3 +1,4 @@
+'''
 # Import the Turtle 2D Graphics Module
 import turtle 
 
@@ -21,3 +22,53 @@ while True:
     if i % 100 == 0:
         print("iteration count: " + str(i))
     i = i + 1
+'''
+
+
+
+# Import the Tkinter & Turtle Graphics Modules
+import Tkinter
+import turtle
+
+'''
+# Create Game Window
+screen = turtle.Screen()
+screen.screensize(600, 600, "green")
+'''
+
+'''
+# Create Game Window
+screen = turtle.Screen()
+screen.setup(600, 600)
+screen.bgcolor("green")
+'''
+screen = turtle.Screen()
+screen.bgcolor("black")
+screen.screensize()
+screen.setup(width = 1.0, height = 1.0)
+
+pixel = []
+newTurtle = turtle.Turtle()
+newTurtle.hideturtle()
+newTurtle.penup()
+newTurtle.goto(-240, 240)
+newTurtle.shape("square")
+newTurtle.color("white")
+newTurtle.showturtle()
+pixel.append(newTurtle)
+
+# Create Sprite
+'''
+screen.addshape("sprite.gif")
+mySprite = turtle.Turtle()
+mySprite.hideturtle() # hide turtle until the the duck image replaces the placeholder shape
+mySprite.penup() # prevent sprite from drawing on screen as it moves
+mySprite.left(90) # rotate sprite for proper placement on screen
+mySprite.shape("sprite.gif") # Set Image for Sprite
+mySprite.setposition(0, 0)
+mySprite.showturtle()
+'''
+
+
+# Main Game Loop (Keeps Game Window From Closing)
+Tkinter.mainloop()
