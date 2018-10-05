d0b21273db4aa3973ba22f66662d6d650d97eb31
Christopher Pedersen <12384786+topherPedersen@users.noreply.github.com>
2018-10-04 22:37:15 -0500

    Update pmode.py

--- pmode.py
+++ pmode.py
@@ -18,79 +18,143 @@ screen.setup(width = 1.0, height = 1.0)
 # Each "pixel" Will Represent a Dot on the Player's Screen
 pixel = []
 pixelIndex = 0
-for x in range(30):
+for x in range(900):
 	pixel.append(True) # Assign Placeholder Value
 
-yCoordinate = 290
-for x in range(30):
-    if x == 0:
-        xCoordinate = -290
-    elif x == 1:
-        xCoordinate = -270
-    elif x == 2:
-        xCoordinate = -250
-    elif x == 3:
-        xCoordinate = -230
-    elif x == 4:
-        xCoordinate = -210
-    elif x == 5:
-        xCoordinate = -190
-    elif x == 6:
-        xCoordinate = -170
-    elif x == 7:
-        xCoordinate = -150
-    elif x == 8:
-        xCoordinate = -130
-    elif x == 9:
-        xCoordinate = -110
-    elif x == 10:
-        xCoordinate = -90
-    elif x == 11:
-        xCoordinate = -70
-    elif x == 12:
-        xCoordinate = -50
-    elif x == 13:
-        xCoordinate = -30
-    elif x == 14:
-        xCoordinate = -10
-    elif x == 15:
-        xCoordinate = 10
-    elif x == 16:
-        xCoordinate = 30
-    elif x == 17:
-        xCoordinate = 50
-    elif x == 18:
-        xCoordinate = 70
-    elif x == 19:
-        xCoordinate = 90
-    elif x == 20:
-        xCoordinate = 110
-    elif x == 21:
-        xCoordinate = 130
-    elif x == 22:
-        xCoordinate = 150
-    elif x == 23:
-        xCoordinate = 170
-    elif x == 24:
-        xCoordinate = 190
-    elif x == 25:
-        xCoordinate = 210
-    elif x == 26:
-        xCoordinate = 230
-    elif x == 27:
-        xCoordinate = 250
-    elif x == 28:
-        xCoordinate = 270
-    elif x == 29:
-        xCoordinate = 290
-    pixel[pixelIndex] = turtle.Turtle()
-    pixel[pixelIndex].hideturtle()
-    pixel[pixelIndex].penup()
-    pixel[pixelIndex].goto(xCoordinate, yCoordinate)
-    pixel[pixelIndex].shape("square")
-    pixel[pixelIndex].color("green")
-    pixel[pixelIndex].showturtle()
-    pixelIndex = pixelIndex + 1
+# yCoordinate = 290
+for y in range(30):
+    # Set yCoordinate
+    if y == 0:
+        yCoordinate = 290
+    elif y == 1:
+        yCoordinate = 270
+    elif y == 2:
+        yCoordinate = 250
+    elif y == 3:
+        yCoordinate = 230
+    elif y == 4:
+        yCoordinate = 210
+    elif y == 5:
+        yCoordinate = 190
+    elif y == 6:
+        yCoordinate = 170
+    elif y == 7:
+        yCoordinate = 150
+    elif y == 8:
+        yCoordinate = 130
+    elif y == 9:
+        yCoordinate = 110
+    elif y == 10:
+        yCoordinate = 90
+    elif y == 11:
+        yCoordinate = 70
+    elif y == 12:
+        yCoordinate = 50
+    elif y == 13:
+        yCoordinate = 30
+    elif y == 14:
+        yCoordinate = 10
+    elif y == 15:
+        yCoordinate = -10
+    elif y == 16:
+        yCoordinate = -30
+    elif y == 17:
+        yCoordinate = -50
+    elif y == 18:
+        yCoordinate = -70
+    elif y == 19:
+        yCoordinate = -90
+    elif y == 20:
+        yCoordinate = -110
+    elif y == 21:
+        yCoordinate = -130
+    elif y == 22:
+        yCoordinate = -150
+    elif y == 23:
+        yCoordinate = -170
+    elif y == 24:
+        yCoordinate = -190
+    elif y == 25:
+        yCoordinate = -210
+    elif y == 26:
+        yCoordinate = -230
+    elif y == 27:
+        yCoordinate = -250
+    elif y == 28:
+        yCoordinate = -270
+    elif y == 29:
+        yCoordinate = -290
+    # Set xCoordinate
+    for x in range(30):
+        if x == 0:
+            xCoordinate = -290
+        elif x == 1:
+            xCoordinate = -270
+        elif x == 2:
+            xCoordinate = -250
+        elif x == 3:
+            xCoordinate = -230
+        elif x == 4:
+            xCoordinate = -210
+        elif x == 5:
+            xCoordinate = -190
+        elif x == 6:
+            xCoordinate = -170
+        elif x == 7:
+            xCoordinate = -150
+        elif x == 8:
+            xCoordinate = -130
+        elif x == 9:
+            xCoordinate = -110
+        elif x == 10:
+            xCoordinate = -90
+        elif x == 11:
+            xCoordinate = -70
+        elif x == 12:
+            xCoordinate = -50
+        elif x == 13:
+            xCoordinate = -30
+        elif x == 14:
+            xCoordinate = -10
+        elif x == 15:
+            xCoordinate = 10
+        elif x == 16:
+            xCoordinate = 30
+        elif x == 17:
+            xCoordinate = 50
+        elif x == 18:
+            xCoordinate = 70
+        elif x == 19:
+            xCoordinate = 90
+        elif x == 20:
+            xCoordinate = 110
+        elif x == 21:
+            xCoordinate = 130
+        elif x == 22:
+            xCoordinate = 150
+        elif x == 23:
+            xCoordinate = 170
+        elif x == 24:
+            xCoordinate = 190
+        elif x == 25:
+            xCoordinate = 210
+        elif x == 26:
+            xCoordinate = 230
+        elif x == 27:
+            xCoordinate = 250
+        elif x == 28:
+           xCoordinate = 270
+        elif x == 29:
+           xCoordinate = 290
+        pixel[pixelIndex] = turtle.Turtle()
+        pixel[pixelIndex].speed(0)
+        pixel[pixelIndex].hideturtle()
+        pixel[pixelIndex].penup()
+        pixel[pixelIndex].goto(xCoordinate, yCoordinate)
+        pixel[pixelIndex].shape("square")
+        pixel[pixelIndex].color("green")
+        pixel[pixelIndex].showturtle()
+        pixelIndex = pixelIndex + 1
 
 # Main Game Loop (Keeps Game Window From Closing)
 tkinter.mainloop()
